dc2e8bc8a580db130f9ca4f2d70849c807ecd9d6
BuildTools <unconfigured@null.spigotmc.org>
2023-11-27 19:13:20 +0100

    [added] Button for showing Talking users

--- src/app.js
+++ src/app.js
@@ -13,6 +13,9 @@ let TeamSpeakPTM = false; // TeamSpeak ptm status
 let micInterval = false; // StreamDeck mic state check to update icon/state
 let soundInterval = false; // StreamDeck output state check to update icon/state
 let afkInterval = false; // StreamDeck afk state check to update icon/state
+let userarray = []; // Array to store all users in the current channel
+let overlaybtncontext;
+let talkingurls = new Set();
 
 // Reconnect methode if a disconnect happens
 const reconnectTeamSpeak = async (apiKey) => {
@@ -57,6 +60,7 @@ const createTeamSpeakSocket = (apiKey) => {
       console.log(data.status.message);
       return;
     }
+    console.log(data);
     // Handle auth events
     if (data.type === "auth") {
       console.log("TeamSpeak -- Auth: ");
@@ -115,7 +119,26 @@ const createTeamSpeakSocket = (apiKey) => {
           TeamSpeakIsAFK = false;
         }
       }
-    } else {
+    }else if(data.type === "talkStatusChanged"){
+        url = userarray[String(data.payload.clientId)]['avatar'].split(";").sort((a,b) => ["2", "3", "4", "1"].indexOf(a[0]) - ["2", "3", "4", "1"].indexOf(b[0]))[0].split(",")[1];
+        if(data.payload.status === 1){
+            console.log(userarray[String(data.payload.clientId)]['user']+" spricht gerade");
+            talkingurls.add(url);
+        }else{
+          console.log(userarray[String(data.payload.clientId)]['user']+" spricht nicht mehr");
+          talkingurls.delete(url);
+        }
+        convertNAvatarsAndOneNumberToImageDataURL(Array.from(talkingurls), '6').then(dataUrl => {
+          $SD.setImage(overlaybtncontext, dataUrl);
+        });
+  
+    }else if(data.type === "clientMoved"){
+        if(data.payload.oldChannelId == "0" && data.payload.properties !== null){
+           userarray[String(data.payload.clientId)] = [];
+           userarray[String(data.payload.clientId)]['user'] = data.payload.properties.nickname;
+           userarray[String(data.payload.clientId)]['avatar'] = data.payload.properties.myteamspeakAvatar;
+        }
+    }else {
       // console.log(data);
     }
   };
@@ -142,6 +165,7 @@ const createTeamSpeakSocket = (apiKey) => {
 const micMute = new Action("de.leonmarcel.teamspeak5.muteaction");
 const soundMute = new Action("de.leonmarcel.teamspeak5.soundmuteaction");
 const afk = new Action("de.leonmarcel.teamspeak5.afkaction");
+const overlaybtn = new Action("de.leonmarcel.teamspeak5.dummyaction"); //Overlay button for talking persons [EDIT THIS TO YOUR NEEDS]
 let settings;
 
 // Saving the APIKey from TeamSpeak into the Elgato settings database
@@ -187,6 +211,11 @@ micMute.onWillAppear(({ context }) => {
   }, 1000);
 });
 
+overlaybtn.onWillAppear(({ context }) => {
+  console.log("Overlay button context: " + context);
+  overlaybtncontext = context;
+});
+
 soundMute.onWillAppear(({ context }) => {
   if (soundInterval) {
     return;
@@ -228,6 +257,10 @@ soundMute.onWillDisappear(({ context }) => {
 afk.onWillDisappear(({ context }) => {
   clearInterval(afkInterval);
   afkInterval = false;
+});
+
+overlaybtn.onWillDisappear(({ context }) => {
+  overlaybtncontext = undefined;
 });
 
 // ----------------------------------------------------
@@ -743,3 +776,99 @@ ptt.onKeyUp(({ action, context, device, event, payload }) => {
     }
   }
 });
+
+
+convertNAvatarsAndOneNumberToImageDataURL = async (urls,n) => {
+  // one images takes up a little over 1/3 of the width (let's say 50px for now)
+  // all images are cut into circles
+  // images overlap by 1/3 of their radius
+  // there will be a max of 6 images
+  // the combination of all images will be centered in a square
+  // the square will be 144 x 144
+
+  if (urls.length < 1) {
+    throw new Error('too few images');
+  }
+  if (urls.length > 6) {
+    urls = urls.slice(0, 6);
+  }
+
+  canvas_size = 144;
+  canvas_center = canvas_size / 2;
+  circle_radius = canvas_size / 6 * 1.05;
+  single_offset = circle_radius / 3 * 2;
+  double_offset = circle_radius / 3 * 5;
+  line_shift = canvas_size * 0.05;
+
+  // create a list of images and their positions
+  images = [];
+  for (var i = 0; i < urls.length; i++) {
+    // position describes the center of the image
+    y = canvas_center + (urls.length > 3) * (i < 3 ? (- single_offset) : single_offset); 
+    x = canvas_center;
+    switch (urls.length) {
+      case 1:
+        break;
+      case 2:
+        x += i == 0 ? (- single_offset) : single_offset;
+        break;
+      case 3:
+        x += i == 0 ? (- double_offset) : ((i == 1) ? 0 : double_offset);
+        break;
+      case 4: case 5: case 6:
+        x += (i % 3) == 0 ? (- double_offset) : ((i % 3) == 1 ? 0 : double_offset);
+        x += i < 3 ? - line_shift : i < 6 ? 0 : line_shift;
+        break;
+    }
+
+    image = new Image();
+    image.src = urls[i];
+    await image.onload;
+
+    images.push({
+      img: image,
+      x: x,
+      y: y,
+    });
+  }
+
+  // create a canvas
+  canvas = document.createElement('CANVAS');
+  ctx = canvas.getContext('2d');
+
+  canvas.height = canvas_size;
+  canvas.width = canvas_size;
+
+  // make background black
+  ctx.fillStyle = '#1c2538';
+  ctx.fillRect(0, 0, canvas_size, canvas_size);
+
+  // draw all images
+  for (var i = 0; i < images.length; i++) {
+    ctx.save();
+
+    // turn image into circle
+    ctx.beginPath();
+    ctx.arc(images[i].x, images[i].y, circle_radius, 0, 2 * Math.PI);
+    ctx.closePath();
+    ctx.clip();
+
+    // draw red background
+    ctx.fillStyle = '#222b30';
+    ctx.fillRect(0, 0, canvas_size, canvas_size);
+    // draw image
+    ctx.drawImage(images[i].img, images[i].x - circle_radius, images[i].y - circle_radius, circle_radius * 2, circle_radius * 2);
+
+    // reset clip
+    ctx.restore();
+  }
+
+  // draw number in top right corner
+  ctx.fillStyle = '#000000';
+  ctx.font = 'bold 24px Arial';
+  ctx.textAlign = 'right';
+  ctx.textBaseline = 'top';
+  ctx.fillText(n, canvas_size, 0);
+
+  return canvas.toDataURL();
+}
